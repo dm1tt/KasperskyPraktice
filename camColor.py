f1ae1e5af1813a628e7fa014accd0f60c3b3c989
dm1tt <Diman_krasnov02@mail.ru>
2022-07-06 23:36:01 +0300

    udpate camColor

--- camColor.py
+++ camColor.py
@@ -1,6 +1,12 @@
 import cv2 
 import numpy as np
 
+def write_file(cent):    
+    file = open("/home/dm1ttry/Рабочий стол/practice/coords.txt", "a")
+    file.write(str(cent) + '\n')
+    file.close()
+
+
 if __name__ == '__main__':
     cv2.namedWindow( "camera" ) #создание окна
     cap = cv2.VideoCapture(0)
@@ -13,33 +19,38 @@ if __name__ == '__main__':
         img = cv2.flip(img,1)
         try:
 
-            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
-            thresh = cv2.inRange(hsv, left, right)      #создание и отрисовка контура
-            contours, hierarchy = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            def cont():
+                hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
+                thresh = cv2.inRange(hsv, left, right)      #создание и отрисовка контура
+                contours, hierarchy = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                return contours
 
-            for cnt in contours:
+            for cnt in cont():
+
+                def center():
+                    rect = cv2.minAreaRect(cnt)
+                    box = cv2.boxPoints(rect)
+                    box = np.int0(box)
+                    center = (int(rect[0][0]),int(rect[0][1]))
+                    area = int(rect[1][0]*rect[1][1])
+                    write_file(center)
+                    return center, area, box
+    
+                cent, ar, box = center()
 
 
-                rect = cv2.minAreaRect(cnt)
-                box = cv2.boxPoints(rect)
-                box = np.int0(box)
-                center = (int(rect[0][0]),int(rect[0][1]))
-                area = int(rect[1][0]*rect[1][1])
+                if ar > 1000: #вывод координат на изображение 
+                    def coords():
 
-                if area > 1000: #вывод координат на изображение 
-
-                    cv2.drawContours(img,[box],0,(255, 0, 0),2)
-
-                    cv2.circle(img, center, 2, (0, 0, 128), 2)
-
-                    text = "(" + str(center[0]) + ", " + str(center[1]) + ")"
-
-                    cv2.putText(img, text, (center[0] + 10, center[1] + 10), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 128), 1, 8, 0)
+                        cv2.drawContours(img,[box],0,(255, 0, 0),2)
+                        cv2.circle(img, cent, 2, (0, 0, 128), 2)
+                        text = "(" + str(cent[0]) + ", " + str(cent[1]) + ")"
+                        cv2.putText(img, text, (cent[0] + 10, cent[1] + 10), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 128), 1, 8, 0)
                     
-                   # print(center)
-                  # f = open("coords.txt", "a")
-                 #  f.write("center")
-                   
+                    coords()
+
+                   # write_file()
+                  
 
 
             cv2.imshow('camera', img)
