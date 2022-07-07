ef2dbd313fa5c03b5e964384b44b6a6d1a54682c
dm1tt <Diman_krasnov02@mail.ru>
2022-07-07 09:51:41 +0300

    update

--- camColor.py
+++ camColor.py
@@ -6,6 +6,26 @@ def write_file(cent):
     file.write(str(cent) + '\n')
     file.close()
 
+def cont():
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
+    thresh = cv2.inRange(hsv, left, right)      #создание и отрисовка контура
+    contours, hierarchy = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    return contours
+
+def center():
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    center = (int(rect[0][0]),int(rect[0][1]))
+    area = int(rect[1][0]*rect[1][1])
+    write_file(center)
+    return center, area, box
+
+def coords():
+    cv2.drawContours(img,[box],0,(255, 0, 0),2)
+    cv2.circle(img, cent, 2, (0, 0, 128), 2)
+    text = "(" + str(cent[0]) + ", " + str(cent[1]) + ")"
+    cv2.putText(img, text, (cent[0] + 10, cent[1] + 10), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 128), 1, 8, 0)
 
 if __name__ == '__main__':
     cv2.namedWindow( "camera" ) #создание окна
@@ -19,42 +39,17 @@ if __name__ == '__main__':
         img = cv2.flip(img,1)
         try:
 
-            def cont():
-                hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
-                thresh = cv2.inRange(hsv, left, right)      #создание и отрисовка контура
-                contours, hierarchy = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-                return contours
-
             for cnt in cont():
 
-                def center():
-                    rect = cv2.minAreaRect(cnt)
-                    box = cv2.boxPoints(rect)
-                    box = np.int0(box)
-                    center = (int(rect[0][0]),int(rect[0][1]))
-                    area = int(rect[1][0]*rect[1][1])
-                    write_file(center)
-                    return center, area, box
-    
+                  
                 cent, ar, box = center()
 
 
                 if ar > 1000: #вывод координат на изображение 
-                    def coords():
-
-                        cv2.drawContours(img,[box],0,(255, 0, 0),2)
-                        cv2.circle(img, cent, 2, (0, 0, 128), 2)
-                        text = "(" + str(cent[0]) + ", " + str(cent[1]) + ")"
-                        cv2.putText(img, text, (cent[0] + 10, cent[1] + 10), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 128), 1, 8, 0)
                     
-                    coords()
-
-                   # write_file()
-                  
-
+                    coords()      
 
             cv2.imshow('camera', img)
-
 
         except:
             cap.release()
