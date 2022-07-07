4bcea7a728d2daea05006da4fce09ac4ec77b6e2
slayenger <73400932+slayenger@users.noreply.github.com>
2022-07-07 23:32:07 +0300

    Add files via upload

---
+++ separate_storage/common/file_names.h
@@ -0,0 +1,8 @@
+/* © 2021 AO Kaspersky Lab */
+#ifndef SEPARATE_STORAGE_FILE_NAMES_H
+#define SEPARATE_STORAGE_FILE_NAMES_H
+
+const char *CertificateFileName = "/c/certificate.cer";
+const char *UserListFileName = "/c/userlist.txt";
+
+#endif
